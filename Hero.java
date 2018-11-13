428ef6ae144fc5596e01daa205a46b499d8b4a7c
J-Vink <44866748+J-Vink@users.noreply.github.com>
2018-11-13 12:00:49 +0100

    3 Werelden toegevoegd, waaronder startscherm, transitiescherm en wereld 1. De speler kan nu niet meer meerdere keren achter elkaar springen. Tevens is er een knop toegevoegd om van het startscherm naar het speelscherm te schakelen.

--- Hero.java
+++ Hero.java
@@ -1,4 +1,3 @@
-
 import greenfoot.*;
 
 /**
@@ -10,13 +9,16 @@ public class Hero extends Mover {
     private final double gravity;
     private final double acc;
     private final double drag;
+    private boolean jumping;
+    public byte karakter = 1;
+    public String wereld = "Wereld_1";
 
     public Hero() {
         super();
         gravity = 9.8;
         acc = 0.6;
         drag = 0.8;
-        setImage("p1.png");
+        setImage("p" + karakter+ ".png");
     }
 
     @Override
@@ -32,21 +34,26 @@ public class Hero extends Mover {
 
         for (Actor enemy : getIntersectingObjects(Enemy.class)) {
             if (enemy != null) {
-                getWorld().removeObject(this);
+                Greenfoot.setWorld(new wereld);
                 break;
             }
         }
     }
-
+    
+    boolean opGrond(){
+        Actor onder = getOneObjectAtOffset(0, getImage().getHeight()/2, Tile.class);
+        return onder != null;
+    }
+    
     public void handleInput() {
-        if (Greenfoot.isKeyDown("space")) {
-            velocityY = -10;
+        if ((Greenfoot.isKeyDown("space")) && (opGrond() == true)) {
+            velocityY = -30;
         }
 
         if (Greenfoot.isKeyDown("left")) {
-            velocityX = -2;
+            velocityX = -10;
         } else if (Greenfoot.isKeyDown("right")) {
-            velocityX = 2;
+            velocityX = 10;
         }
     }
 
---
+++ Startscherm.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Startscherm here.
+ * 
+ * @author (Jeffrey Vink) 
+ * @version (a version number or a date)
+ */
+public class Startscherm extends World
+{
+
+    /**
+     * Constructor for objects of class Startscherm.
+     * 
+     */
+    public Startscherm()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1000, 800, 1); 
+        this.setBackground("bg.png");
+        Start start = new Start();
+        addObject(start, 500, 650);
+    }
+    
+    
+}
